783e3a7547dc12248a204f190f74cb911c17471f
fwfan <vivoic@163.com>
2022-11-04 15:05:48 +0800

    init

---
+++ html/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        用于显示UI 代码
+      </header>
+    </div>
+  );
+}
+
+export default App;
