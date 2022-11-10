e527644b0c0e0f47d971876662365da9517a5f32
fwfan <vivoic@163.com>
2022-11-10 10:46:48 +0800

    build

---
+++ html/build/static/js/385.0ef1350f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkhtml=self.webpackChunkhtml||[]).push([[385],{385:function(n,t,e){e.r(t);var r=e(671),u=e(144),i=e(136),s=e(104),h=e(791),a=e(184),c=function(n){(0,i.Z)(e,n);var t=(0,s.Z)(e);function e(){return(0,r.Z)(this,e),t.apply(this,arguments)}return(0,u.Z)(e,[{key:"render",value:function(){return(0,a.jsx)(a.Fragment,{children:"this is About!"})}}]),e}(h.PureComponent);t.default=c}}]);
+//# sourceMappingURL=385.0ef1350f.chunk.js.map
