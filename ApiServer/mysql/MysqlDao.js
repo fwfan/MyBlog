4f87ad883e22c88791cc7eadc3bdb02f8e3dd0c1
fwfan <vivoic@163.com>
2022-11-29 20:11:29 +0800

    Update MysqlDao.js

--- ApiServer/mysql/MysqlDao.js
+++ ApiServer/mysql/MysqlDao.js
@@ -2,6 +2,7 @@ const mysql = require("mysql");
 let connection = mysql.createConnection({
   host: "182.61.5.126",
   user: "root",
+  password: "f110112119!"
 });
 
 exports.connection = connection;
