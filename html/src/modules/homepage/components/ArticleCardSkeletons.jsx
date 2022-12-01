86652ec47266810b0454a727029ff3f833f678fc
fwfan <vivoic@163.com>
2022-12-01 14:15:52 +0800

    Seleton updated

--- html/src/modules/homepage/components/ArticleCardSkeletons.jsx
+++ html/src/modules/homepage/components/ArticleCardSkeletons.jsx
@@ -2,12 +2,14 @@ import { Skeleton } from "antd";
 const ArticleCardSkeletons = (props) => {
   const skeletonsArr = [];
   for (let i = 0; i < props.num; i++) {
-    skeletonsArr.push({ active: true, title: true ,key :i});
+    skeletonsArr.push({ active: true, title: true, key: i });
   }
   return (
     <>
       {skeletonsArr.map((item) => (
-        <Skeleton {...item}/>
+        <div style={{ margin: "12px 0px 0px 0px" }}>
+          <Skeleton {...item} />
+        </div>
       ))}
     </>
   );
