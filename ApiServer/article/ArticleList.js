2b2b0237516d62252ec7ebb75f18587d7cdeaff1
fwfan <vivoic@163.com>
2022-12-07 14:38:50 +0800

    Update node mysql

--- ApiServer/article/ArticleList.js
+++ ApiServer/article/ArticleList.js
@@ -1,3 +1,4 @@
+const { query } = require("express");
 const express = require("express");
 const router = express.Router();
 const MysqlDao = require("../mysql/MysqlDao");
@@ -7,19 +8,32 @@ const connection = MysqlDao.connection;
 
 router.get("/article_list", (req, res) => {
   const { start, limit } = Utils.parseQuery(req.query);
-  // connection.connect();
-  connection.query(
-    "SELECT id, title, author, summary, sub_type, create_time, total_view FROM blog.article LIMIT ?, ?",
-    [start, limit],
-    function (err, result) {
-      
-      if (err) {
-        res.send(Utils.parseFailedResult(err.message));
-        return;
+
+  const execRes =(info)=>{
+    connection.end();
+    res.send(info);
+  }
+  const queryData =(sta, lim, callback)=>{
+    connection.query(
+      "SELECT id, title, author, summary, sub_type, create_time, total_view FROM blog.article LIMIT ?, ?",
+      [sta, lim],
+      function (err, result) {
+        if (err) {
+          callback(Utils.parseFailedResult(err.message));
+          return;
+        }
+        callback(Utils.parseSuccessResult(result, result.length))
       }
-      res.send(Utils.parseSuccessResult(result, result.length));
-    }
-  );
+    );
+  }
+
+  connection.connect();
+  queryData(start, limit, execRes);
+
+
+  
+
+
   // connection.end();
   
 });
